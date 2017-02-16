4f2fee662f0683eeb8a21f7eb0c5a48170419975
zuiidea <zuiiidea@gmail.com>
2017-02-16 14:29:54 +0800

    rebuild

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-"# seajs-example" 
+# seajs-cli
+- 基于[seajs](https://github.com/nuysoft/Mock) 的模块管理，热重载，模块打包的脚手架
+
+### 快速开始
+
+克隆项目文件:
+
+```
+git clone git@github.com:zuiidea/seajs-cli.git
+```
+
+进入目录安装依赖:
+
+```
+npm i
+```
+
+开发：
+
+```bash
+gulp
+
+先编译一次，再监听，打开 http://localhost:3000
+
+gulp dev
+
+无编译，直接监听
+```
+
+清理：
+
+```bash
+gulp clean
+
+将会清理dist目录
+```
+
+构建：
+
+```bash
+gulp build
+
+先清理dist目录，再打包压缩生成dist目录
+```
--- gulpfile.js
+++ gulpfile.js
@@ -9,72 +9,73 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   plumber = require('gulp-plumber'),
   seajsCombo = require('gulp-seajs-combo'),
-  _task = require('./gulp-task'),
-  config = require('./gulpconfig.json')
+  watch = require('gulp-watch'),
+  config = require('./gulpConfig.json')
 
 // css
 gulp.task('css', function () {
   return gulp.src(config.src.css)
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(minifycss())
-        .pipe(gulp.dest(config.dest.css))
+  .pipe(watch(config.src.css))
+  .pipe(plumber())
+  .pipe(less())
+  .pipe(autoprefixer())
+  .pipe(minifycss())
+  .pipe(gulp.dest(config.dest.css))
 })
 
 gulp.task('css-min', ['css'], function () {
   return gulp.src(config.res.css)
-        .pipe(minifycss())
-        .pipe(gulp.dest(config.dest.css))
+  .pipe(minifycss())
+  .pipe(gulp.dest(config.dest.css))
 })
 
 // js
-
 var modulePath = '../module/'
 gulp.task('entry', function () {
   return gulp.src(config.src.entry)
-        .pipe(seajsCombo({
-          map: {
-            'swiper': modulePath + 'swiper/swiper.js',
-            'layer': modulePath + 'layer/layer.js',
-            'flexible': modulePath + 'flexible/flexible.js',
-            'fastclick': modulePath + 'fastclick/fastclick.js'
-          }
-        }))
-        .pipe(rename({
-          basename: 'build'
-        }))
-        .pipe(gulp.dest(config.dest.js))
+  .pipe(seajsCombo({
+    map: {
+      'swiper': modulePath + 'swiper/swiper.js',
+      'layer': modulePath + 'layer/layer.js',
+      'flexible': modulePath + 'flexible/flexible.js',
+      'fastclick': modulePath + 'fastclick/fastclick.js'
+    }
+  }))
+  .pipe(rename({
+    basename: 'build'
+  }))
+  .pipe(gulp.dest(config.dest.js))
 })
 
 gulp.task('baseJs', function () {
   return gulp.src(config.src.baseJs)
-        .pipe(concat('base.js'))
-        .pipe(gulp.dest(config.dest.js))
+  .pipe(concat('base.js'))
+  .pipe(gulp.dest(config.dest.js))
 })
 
 gulp.task('js', ['entry', 'baseJs'], function () {
-
+  gulp.start('entry', 'baseJs')
 })
 
 gulp.task('js-min', ['js'], function () {
   return gulp.src(config.res.js)
-        .pipe(uglify())
-        .pipe(gulp.dest(config.dest.js))
+  .pipe(uglify())
+  .pipe(gulp.dest(config.dest.js))
 })
 
 // img
 gulp.task('img', function () {
   return gulp.src(config.src.img)
-        .pipe(plumber())
-        .pipe(gulp.dest(config.dest.img))
+  .pipe(watch(config.src.img))
+  .pipe(plumber())
+  .pipe(gulp.dest(config.dest.img))
 })
 
 // html
 gulp.task('html', function () {
   return gulp.src(config.src.html)
-        .pipe(plumber())
-        .pipe(gulp.dest(config.dest.html))
+  .pipe(plumber())
+  .pipe(gulp.dest(config.dest.html))
 })
 
 // watch
@@ -86,6 +87,7 @@ gulp.task('browserSync', function () {
       config.dest.img,
       config.dest.html
     ],
+    open: false,
     server: {
       baseDir: './dist/'
     }
@@ -97,25 +99,22 @@ gulp.task('clean', function () {
   return gulp.src(config.src.clean, {
     read: false
   })
-        .pipe(clean())
+  .pipe(clean())
 })
 
-gulp.task('dev', ['css', 'js', 'html', 'img', 'browserSync'], function () {
-  var watcher = gulp.watch(config.src.css, ['css'])
+gulp.task('dev', ['browserSync'], function () {
+  gulp.watch(config.src.css, ['css'])
   gulp.watch(config.src.html, ['html'])
   gulp.watch(config.src.img, ['img'])
-  gulp.watch(config.src.js, ['js'])
-  watcher.on('change', function (event) {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...')
-  })
+  gulp.watch(config.src.js, ['entry'])
+  gulp.watch(config.src.baseJs, ['baseJs'])
 })
 
-gulp.task('default', ['dev'], function () {
-  _task({
-    projectName: 'seajsExample'
-  })
+gulp.task('default',function () {
+  gulp.start('css', 'html', 'img', 'js')
+  gulp.start('dev')
 })
 
-gulp.task('pub', ['clean'], function () {
+gulp.task('build', ['clean'], function () {
   gulp.start('css-min', 'js-min', 'html', 'img')
 })
